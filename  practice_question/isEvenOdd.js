14d628603553ac59dc941f39a27d6959afd1972a
alokmotion <alokpatel406@gmail.com>
2025-11-22 08:30:08 +0530

    even odd

---
+++  practice_question/isEvenOdd.js
@@ -0,0 +1,11 @@
+// create a function to check number is even or odd
+
+function checkNumber(num) {
+    if (num % 2 == 0) {
+        console.log("this number is even")
+    } else {
+        console.log("this numbe is odd")
+    }
+}
+
+checkNumber(6)
